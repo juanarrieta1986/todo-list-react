49b804761fd88d654d85a1dcff936a32eb0b3b88
juanarrieta1986 <juanca86@gmail.com>
2021-03-10 05:46:10 +0000

    v2

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 
-
 export function Home() {
-    
-    const [todoList, setList] = useState([]);
-    
-    /*function addTask () {
-        //setList[[...todoList,]]
-        let vartest;
-    }*/
+	const [todoList, setList] = useState([]);
+	const [todoInput, setInput] = useState("");
+	let listElement;
+	let listCopy = [];
+
+	const handleRemoveItem = e => {
+		const index = e.target.parentElement.getAttribute("data-index");
+
+		listCopy = todoList;
+		listCopy = todoList.splice(index, 1);
+		setList(todoList.filter(item => item.name !== listCopy));
+	};
+
 	return (
 		<div id="container">
 			<h1 className="todo-header">To do List</h1>
@@ -23,37 +29,35 @@ export function Home() {
 				placeholder="Add to do here"
 				onKeyDown={e => {
 					if (e.keyCode === 13 && e.target.value != "") {
-						//checks whether the pressed key is "Enter"
-                        setList[[...todoList,e.target.value]]
-						/*var list = document.querySelector("ul");
-						var newLiElem = document.createElement("LI");
-						newLiElem.innerHTML =
-							"<span><i className='fa fa-trash'></i></span> " +
-							e.target.value;
-						document.querySelector("UL").appendChild(newLiElem);*/
+						console.log(e.target.value);
+						setList([...todoList, e.target.value]);
+						e.target.value = "";
 					}
 				}}
 			/>
-			<ul>
-				<li>
-					<span>
-						<i className="fa fa-trash"></i>
-					</span>{" "}
-					Eat
-				</li>
-				<li>
-					<span>
-						<i className="fa fa-trash"></i>
-					</span>{" "}
-					Drink
-				</li>
-				<li>
-					<span>
-						<i className="fa fa-trash"></i>
-					</span>{" "}
-					Sleep
-				</li>
+			<ul className="list-group">
+				{todoList.map((item, index) => {
+					return (
+						<li
+							key={index}
+							data-index={index}
+							className="list-group-item">
+							<i
+								className="fa fa-trash"
+								onClick={handleRemoveItem}></i>
+							{"   -   " + item}
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
 }
+
+/*listElem.addEventListener("click", function (e) {
+    console.log(e);
+    let elemToDelete = e.target.parentElement.parentElement;
+    elemToDelete.className = "deleteLI";
+    var element = listElem.getElementsByClassName("deleteLI");
+    listElem.removeChild(elemToDelete);
+});*/
