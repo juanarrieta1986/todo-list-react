a27b61852d7027a9bb9fdc839c8be19db4e8ee92
juanarrieta1986 <juanca86@gmail.com>
2021-03-10 02:09:49 +0000

    v1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,21 +4,56 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
+
 export function Home() {
+    
+    const [todoList, setList] = useState([]);
+    
+    /*function addTask () {
+        //setList[[...todoList,]]
+        let vartest;
+    }*/
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="container">
+			<h1 className="todo-header">To do List</h1>
+			<input
+				id="addToDo"
+				type="text"
+				placeholder="Add to do here"
+				onKeyDown={e => {
+					if (e.keyCode === 13 && e.target.value != "") {
+						//checks whether the pressed key is "Enter"
+                        setList[[...todoList,e.target.value]]
+						/*var list = document.querySelector("ul");
+						var newLiElem = document.createElement("LI");
+						newLiElem.innerHTML =
+							"<span><i className='fa fa-trash'></i></span> " +
+							e.target.value;
+						document.querySelector("UL").appendChild(newLiElem);*/
+					}
+				}}
+			/>
+			<ul>
+				<li>
+					<span>
+						<i className="fa fa-trash"></i>
+					</span>{" "}
+					Eat
+				</li>
+				<li>
+					<span>
+						<i className="fa fa-trash"></i>
+					</span>{" "}
+					Drink
+				</li>
+				<li>
+					<span>
+						<i className="fa fa-trash"></i>
+					</span>{" "}
+					Sleep
+				</li>
+			</ul>
 		</div>
 	);
 }
