c8e10308507241dc1e56b1d352c7fc59c0023074
juanarrieta1986 <juanca86@gmail.com>
2021-03-11 04:39:27 +0000

    v7

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,17 +7,48 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 
 export function Home() {
-	const [todoList, setList] = useState([]);
+	const [todoList, setList] = useState(["Add Task"]);
 	const [todoInput, setInput] = useState("");
 	let listElement;
 	let listCopy = [];
 
 	const handleRemoveItem = e => {
-		const index = e.target.parentElement.getAttribute("data-index");
+		const liElement = e.target;
+		const index = liElement.parentElement.getAttribute("data-index");
+		//const elementText = liElement.innerText;
 
 		listCopy = todoList;
 		listCopy = todoList.splice(index, 1);
 		setList(todoList.filter(item => item.name !== listCopy));
+		console.log(todoList.length);
+		/*if (todoList.length > 1) {
+			liElement
+				.getElementsByTagName("i")[0]
+				.setAttribute("style", "display: inline");
+		}*/
+	};
+	const handleMouserOver = e => {
+		const liElement = e.target; //.getElementsByTagName("i")[0];
+		const elementClass = liElement.className;
+		if (
+			elementClass !== "fa fa-trash" &&
+			liElement.innerText !== "Add Task"
+		) {
+			liElement
+				.getElementsByTagName("i")[0]
+				.setAttribute("style", "display: inline");
+		}
+		console.log(liElement);
+	};
+	const handleMouserOut = e => {
+		const liElement = e.target; //.getElementsByTagName("i")[0];
+		const elementClass = liElement.className;
+		if (elementClass !== "fa fa-trash") {
+			liElement
+				.getElementsByTagName("i")[0]
+				.setAttribute("style", "display: none");
+		}
+		console.log(liElement);
 	};
 
 	return (
@@ -41,11 +72,20 @@ export function Home() {
 						<li
 							key={index}
 							data-index={index}
-							className="list-group-item">
+							className="list-group-item"
+							//style={{ display: "inline" }}
+							style={
+								todoList.length > 1 && index == 0
+									? { display: "none" }
+									: { display: "inline" }
+							}
+							onMouseOver={handleMouserOver}
+							onMouseOut={handleMouserOut}>
 							<i
 								className="fa fa-trash"
+								style={{ display: "none" }}
 								onClick={handleRemoveItem}></i>
-							{"   -   " + item}
+							{item}
 						</li>
 					);
 				})}
@@ -53,11 +93,3 @@ export function Home() {
 		</div>
 	);
 }
-
-/*listElem.addEventListener("click", function (e) {
-    console.log(e);
-    let elemToDelete = e.target.parentElement.parentElement;
-    elemToDelete.className = "deleteLI";
-    var element = listElem.getElementsByClassName("deleteLI");
-    listElem.removeChild(elemToDelete);
-});*/
